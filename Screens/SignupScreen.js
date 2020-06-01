ea0a45462853b3df9f2ea4aaab2176eb38777bd8
Ashley-Lau <ashleylau451@gmail.com>
2020-06-02 00:01:02 +0800

    testing donkey

--- Screens/SignupScreen.js
+++ Screens/SignupScreen.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     StyleSheet,
     Text,
     View,
     Image,
     TextInput,
-    Button,
+    Picker,
     Alert,
     ImageBackground,
     Keyboard,
     TouchableWithoutFeedback
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+
+import { useNavigation} from '@react-navigation/native';
 import * as Animatable from 'react-native-animatable';
 
 import Styles from "../styling/Styles";
 import GradientButton from "../Components/GradientButton";
 
+const SignUpComponent = props => {
+    return <View style = {{marginTop: 10, width: 300}}>
+                <Text style = {{fontSize: 15, fontWeight: 'bold'}}>{props.title}</Text>
+                <TextInput  style = {{marginTop: 0, fontSize:20, borderBottomWidth: 2, borderBottomColor: 'black' }}
+                            placeholderTextColor = '#708090' {...props}/>
+            </View>
+}
+
 const SignupScreen = props => {
     const navigation = useNavigation()
+    const [gender, setGender] = useState('')
+    const [date, setDate] = useState(new Date())
+
+    let data = [{
+        value: 'Male',
+    }, {
+        value: 'Female',
+    }];
     const registeredAlert = () => {
         Alert.alert(
             "Account Registered!",
@@ -37,16 +54,32 @@ const SignupScreen = props => {
                 <View style = {style.empty}/>
                 {/*<View style = {style.popout}>*/}
                 <Animatable.View style = {style.popout} animation = "fadeInUpBig">
-                    <TextInput style={Styles.login}
-                               placeholder="Name"
-                        // onChangeText={}
-                        // value = {}
-                    />
-                    <TextInput style={Styles.login}
-                               placeholder="Password"
-                        // onChangeText={}
-                        // value = {}
-                    />
+                    <Text style = {{fontSize: 30, borderBottomWidth: 2, borderBottomColor: 'black'}}>
+                        Sign Up
+                    </Text>
+                    <SignUpComponent title = 'First Name:' placeholder = "First Name">
+                    </SignUpComponent>
+                    <SignUpComponent title = 'Last Name:' placeholder = "Last Name">
+                    </SignUpComponent>
+                    <SignUpComponent title = 'Username:' placeholder = "6 - 16 characters">
+                    </SignUpComponent>
+                    <SignUpComponent title = 'Password:' placeholder = "Password" secureTextEntry = {true}>
+                    </SignUpComponent>
+                    <SignUpComponent title = 'Confirm Passwords:' placeholder = "Re-Enter Password" secureTextEntry = {true}>
+                    </SignUpComponent>
+                    <View style = {{flexDirection: 'row', marginRight: 100,}}>
+                        <Text style = {{fontSize: 20, fontWeight: 'bold',marginTop: 20}}>Gender: </Text>
+                        <View style = {style.dropDown}>
+                            <Picker
+                                selectedValue={gender}
+                                style={{ height: 50, width: 130, }}
+                                onValueChange={(itemValue, itemIndex) => setGender(itemValue)}
+                            >
+                                <Picker.Item label="Male" value="Male" />
+                                <Picker.Item label="Female" value="Female" />
+                            </Picker>
+                        </View>
+                    </View>
                     <View style={Styles.buttonContainer}>
                         <GradientButton onPress={registeredPress}
                                         style={style.button}
@@ -80,5 +113,13 @@ const style = StyleSheet.create({
     },
     empty: {
         flex: 1,
+    },
+    dropDown: {
+        marginTop: 18,
+        justifyContent: 'center',
+        backgroundColor: 'ghostwhite',
+        height: 33,
+        borderWidth: 1,
+        width: 120,
     }
 })
