9c4148bf39f9c91f801786a2869594ca16218be3
NiMadeNai <e0424663@u.nus.edu>
2020-06-02 22:44:40 +0800

    Edited the ProfilePage, added a new button to push up the modal for hosting game HostGameItem.js is the item that holds the modal being pushed up

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Text, TextInput, Button, Alert} from 'react-native';
+import {View, StyleSheet, Text, TextInput, Button, Alert, Modal} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 import Background from "../views/Background";
 import GradientButton from "../Components/GradientButton";
 import CustButton from "../Components/CustButton";
-import {useNavigation} from "@react-navigation/native";
+import HostGameItem from "../Components/HostGameItem";
+
 
 const ProfileScreen = props => {
     const navigation = useNavigation();
+    const[hostGame, setHostGame] = useState(false);
 
     const confirmLogOut = () => {
         Alert.alert("Confirm Log Out",
@@ -24,9 +27,18 @@ const ProfileScreen = props => {
     }
     return <Background style = {{justifyContent: 'center'}}>
                 <View style = {{}}>
+
                     <GradientButton style={{}} colors = {['#30cfd0','#330867']}>
                         <Text>ProfileScreen</Text>
                     </GradientButton>
+
+                    <HostGameItem visible={hostGame} closeHost={() =>setHostGame(false)}/>
+                    <GradientButton style={{}}
+                                    colors = {['#30cfd0','#330867']}
+                                    onPress={() => setHostGame(true)}>
+                        <Text>Host a Game</Text>
+                    </GradientButton>
+
                     <CustButton style={{marginTop:10,backgroundColor: '#dda0dd', marginHorizontal: 60}}
                                 onPress = {confirmLogOut}>
                         <Text>Log Out</Text>
