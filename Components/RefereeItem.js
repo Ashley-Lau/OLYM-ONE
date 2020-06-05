de447ac5272d185503b342f3b61799623cce3237
NiMadeNai <e0424663@u.nus.edu>
2020-06-05 22:15:04 +0800

    edited the refereescreen, added RefereeItem.js which contains the details and game items of the referees

---
+++ Components/RefereeItem.js
@@ -0,0 +1,139 @@
+import React, {useState} from 'react';
+import {
+    View,
+    StyleSheet,
+    Text,
+    Modal,
+    ScrollView,
+    TouchableOpacity,
+} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Background from "../views/Background";
+import GradientButton from "../Components/GradientButton";
+
+
+const RefereeItem = props => {
+
+    const[openItem ,setOpenItem] = useState(false);
+
+    const refereeItem = <Modal visible={openItem}>
+        <Background>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style = {{alignItems: 'center', paddingBottom: 30,}}>
+                    <View style = {{...styles.elevatedComponent, height: 200, justifyContent: 'space-evenly'}}>
+                        <View style = {{flexDirection: 'column', justifyContent: 'space-around', paddingTop: 5,}}>
+                            <View style={{flexDirection:"row", alignItems:"center", justifyContent:"flex-start", marginLeft:20}}>
+                                <View style = {styles.photoFrame}>
+                                    <MaterialCommunityIcons name='account' size={50}/>
+                                </View>
+                                <View style = {{alignItems: 'center'}}>
+                                    <Text style = {{fontSize: 20}}> Name: {props.title[1]} </Text>
+                                    <Text style = {{fontSize: 20}}> Sport: {props.title[0]} </Text>
+                                </View>
+                            </View>
+
+                            <View style={{flexDirection:"row", justifyContent:"space-around"}}>
+                                <GradientButton style={{width: 120, height:37, marginTop: 20,}}
+                                                colors = {["red", "maroon"]}
+                                                textStyle = {{fontSize: 15}}
+                                                onPress={() => setOpenItem(false)}>
+                                    CANCEL
+                                </GradientButton>
+                                <GradientButton style={{width: 120, height:37, marginTop: 20,}}
+                                                colors = {['#1bb479','#026c45']}
+                                                onPress = {() => setOpenItem(false)}
+                                                textStyle = {{fontSize: 15}}>
+                                    HIRE
+                                </GradientButton>
+                            </View>
+                        </View>
+
+                    </View>
+                    <View style = {{...styles.elevatedComponent, marginTop: 20, height: 400}}>
+                        <View style = {styles.titleBackground} >
+                            <Text style ={styles.titleText}>
+                                Upcoming Games
+                            </Text>
+                        </View>
+                    </View>
+
+                </View>
+            </ScrollView>
+        </Background>
+
+    </Modal>
+
+    return(
+        <View>
+            {refereeItem}
+            <TouchableOpacity style={styles.games}
+                              onPress={() => {setOpenItem(true);}}>
+                {/*to replace icon with the profile picture of the referee*/}
+                <MaterialCommunityIcons name="account" size={35}/>
+                <View style={{flexDirection:"column", marginLeft:15}}>
+                    <Text style={{fontSize:18}}> Name: {props.title[1]}</Text>
+                    <Text style={{fontSize:18, color: "black"}}> {props.title[0]} </Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        justifyContent: 'center',
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    elevatedComponent: {
+        width: '90%',
+        height: 150,
+        elevation: 10,
+        backgroundColor: 'white',
+        marginTop: 40,
+        borderRadius:10,
+    },
+    titleBackground: {
+        backgroundColor: 'green',
+        height: 40,
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+    },
+    titleText: {
+        textDecorationLine: 'underline',
+        fontSize: 25,
+        marginTop: 2,
+        marginLeft: 4,
+        fontWeight: '500',
+        color: 'white',
+    },
+    photoFrame: {
+        height: 85,
+        width: 85,
+        borderRadius: 170,
+        elevation: 10,
+        justifyContent: 'center',
+        alignItems:'center',
+        borderWidth: 2,
+        backgroundColor: 'white',
+    },
+    games:{
+        flexDirection:"row",
+        borderBottomWidth:1,
+        padding:5,
+        justifyContent:"flex-start",
+        alignItems:"center",
+        backgroundColor:"transparent",
+    },
+
+})
+
+export default RefereeItem;
--- Screens/RefereeScreen.js
+++ Screens/RefereeScreen.js
@@ -1,49 +1,110 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Text, TextInput, Button, Alert} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
+import {View, TextInput, StyleSheet, FlatList,} from 'react-native';
 
 import Background from "../views/Background";
-import CustButton from "../Components/CustButton";
-
+import SearchButtons from "../Components/SearchButtons";
+import RefereeItem from "../Components/RefereeItem";
 
 const RefereeScreen = props => {
-    const navigation = useNavigation();
 
-    const confirmLogOut = () => {
-        Alert.alert("Confirm Log Out",
-            "Do you want to log out?",
-            [{
-                text: "Yes",
-                onPress: () => navigation.navigate('LoginScreen'),
-                style: 'cancel'
-            },
-                {text:"Cancel", onPress: () => {},  style:'cancel'}
-                ],
-            {cancelable: false}
-        )
+    // to be replaced with the Firebase db
+    const [refereeList, setRefereeList] = useState([
+        {key: 0, value: ["Soccer", "Ashley"]},
+        {key: 1, value: ["BasketBall", "Dennis"]},
+        {key: 2, value: ["Badminton", "Lum Jian Yang"]},
+        {key: 3, value: ["Floorball", "Kenny Seeeeeet"]},
+        {key: 4, value: ["Badminton", "JoeAlpharius"]},
+        {key: 5, value: ["Soccer", "KeaneChan"]}
+    ]);
+
+    const [filteredList, findFilteredList] = useState([...refereeList]);
+    const [searching, findSearching] = useState("");
+
+    const filteredGames = (searchItem) => {
+        findFilteredList([...filteredList, searchItem]);
+    }
+
+    //need to refine search method
+    const filterList = () => {
+        //not sure why resetting onPress doesnt work
+        findFilteredList([]);
+
+        let filtering = refereeList.map(a => a.value);
+        for (var i = 0; i < filtering.length; i++) {
+            for (var j = 0; j < filtering[i].length; j++) {
+                if(typeof filtering[i][j] === "string"){
+                    if (searching.toLowerCase() == filtering[i][j].toLowerCase()) {
+                        filteredGames(refereeList[i]);
+                        break;
+                    };
+                };
+            };
+        };
 
     }
 
-    return<Background style = {styles.container}>
-                <CustButton style={{backgroundColor: '#dda0dd', marginHorizontal: 60}}>
-                    <Text>RefereeScreen</Text>
-                </CustButton>
-            </Background>
+    const searchHandler = (enteredSearch) => {
+        findSearching(enteredSearch);
+        findFilteredList([]);
+    }
+
+    return (<Background style = {styles.container}>
+            <View style={styles.searchSpace}>
+                <View style={styles.searchBar}>
+                    <TextInput style={styles.searchInput}
+                               placeholder=" Keywords, Referee Name, Sport"
+                               placeholderTextColor="#B9B9B9"
+                               onChangeText={searchHandler}
+                               value={searching}
+                    />
+                    <SearchButtons style={{flex: 1, elevation: 5}} searchMe={filterList}/>
+                </View>
+
+            </View>
+
+            <View style={{justifyContent: "space-around", marginTop:10}}>
+                <FlatList
+                    contentContainerStyle={{justifyContent: "space-between"}}
+                    keyExtractor={(item) => item.key.toString()}
+                    data={filteredList}
+                    renderItem={({item}) => <RefereeItem title={item.value}/>}
+                />
+            </View>
+        </Background>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        // marginTop:36,
+        justifyContent: 'flex-start',
         flexDirection:"column"
+    },
+    searchBar:{
+        // marginTop: 20,
+        flexDirection: "row",
+        justifyContent:"space-between",
+        alignItems:"center",
+        borderWidth:1,
+        borderRadius:4,
+        width:"98%",
+        marginTop:36,
+        marginBottom:10
+    },
+    searchInput:{
+        width:"85%",
+        height:45,
+        fontSize:20,
 
     },
-    text: {
-        justifyContent: 'center',
-        fontSize: 20,
-        fontWeight: "bold",
-    },
+    searchSpace:{
+        justifyContent:"center",
+        alignItems:"center",
+        borderBottomWidth:1,
+        borderBottomColor:"rgba(177,177,177,0.78)"
+    }
+
 })
 
 export default RefereeScreen;
